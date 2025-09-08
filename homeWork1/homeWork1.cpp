929b9932d0d05c89a5340586f5e02a0142a3a362
Arman Xanzadyan <armanxanzadyan32@gmail.com>
2025-09-08 12:45:36 +0400

    Solve this exercise

--- homeWork1/homeWork1.cpp
+++ homeWork1/homeWork1.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
 
-void print(const int* array, const int size);
-
-
 int
 main()
 {
-    int array[] = {2, 4, 8, 23, 987, 8, 2, 4, 987};
+    int array[] = {1, 2, 3, 4, 5, 4, 3, 2, 1};
+    const size_t size = sizeof(array) / sizeof(array[0]);
+
+    int result = 0;
+    for (int i = 0; i < size; ++i) {
+        result ^= array[i];
+    }
+
+    std::cout << "Unique number: " << result << std::endl;
     return 0;
 }
 
-void
-print(const int* array, const int size)
-{
-    for (size_t i = 0; i < size; ++i) {
-        std::cout << *(array + i);
-    }
-}
