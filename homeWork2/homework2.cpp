0305a8f490d48127f2bcfb587ac9dc14138078e1
Arman Xanzadyan <armanxanzadyan32@gmail.com>
2025-09-12 13:05:40 +0400

    Arman Xanzadyan- homework2

---
+++ homeWork2/homework2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int
+main()
+{
+    int array[] = {1, 2, 3, 9, 4, 2, 1, 9};
+    int result = 0;
+    for (int i = 0; i < 8; ++i) {
+        result ^= array[i];
+    }
+    int a = 0;
+    int b = 0;
+    for (int i = 0; i < 8; ++i) {
+        if (array[i] & result != 0) {
+            a ^= array[i];
+            continue;
+        }
+        b ^= array[i];
+    }
+    std::cout << "a = " << a << '\n' << "b = " << b << '\n'; 
+    return 0;
+}
+
